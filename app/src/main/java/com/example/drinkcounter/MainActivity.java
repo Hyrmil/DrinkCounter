614a6d3ade02e1b2548a664cf8aec812648f51bf
hyrmil <bodin.coline@gmail.com>
2021-02-09 00:31:47 +0100

    ref: review logic with new facing

--- app/src/main/java/com/example/drinkcounter/MainActivity.java
+++ app/src/main/java/com/example/drinkcounter/MainActivity.java
@@ -6,51 +6,61 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private Integer count = 0;
-    private TextView viewCount;
+    private TextView viewCount1;
+    private TextView viewCount2;
+    private TextView viewCount3;
+    private TextView viewCount4;
+    private TextView viewDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewCount = (TextView) findViewById(R.id.count);
+        viewCount1 = (TextView) findViewById(R.id.count1);
+        viewCount2 = (TextView) findViewById(R.id.count2);
+        viewCount3 = (TextView) findViewById(R.id.count3);
+        viewCount4 = (TextView) findViewById(R.id.count4);
+        //TODO: switch case button ?
+        viewDate = (TextView) findViewById(R.id.dateStamp);
+        setDate(viewDate);
     }
 
-    public void countWater(View view) {
-        count++;
-        if (viewCount != null) {
-            viewCount.setText(String.format("%d verre(s)",count));
-        }
-    }
+    private void setDate(TextView viewDate) {
 
-    public void countHotTea(View view) {
-        count++;
-        if (viewCount != null) {
-            viewCount.setText(String.format("%d verre(s)",count));
-        }
-    }
-
-    public void countIceTea(View view) {
-        count++;
-        if (viewCount != null) {
-            viewCount.setText(String.format("%d verre(s)",count));
-        }
-    }
-
-    public void countMilk(View view) {
-        count++;
-        if (viewCount != null) {
-            viewCount.setText(String.format("%d verre(s)",count));
-        }
+        Date today = Calendar.getInstance().getTime();//getting date
+        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, Locale.FRANCE);
+        String date = df.format(today);
+        viewDate.setText(date);
     }
 
     public void resetCount(View view) {
         count = 0;
-        if (viewCount != null) {
-            viewCount.setText(String.format("%d verre",count));
+        if (viewCount1 != null) {
+            viewCount1.setText(Integer.toString(count));
+        }
+    }
+
+    public void sub_counter(View view) {
+        count--;
+        if (viewCount1 != null) {
+            viewCount1.setText(Integer.toString(count));
+        }
+    }
+
+    public void add_counter(View view) {
+        count++;
+        if (viewCount1 != null) {
+            viewCount1.setText(Integer.toString(count));
         }
     }
 }
