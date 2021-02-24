793140f07b9c39bce2e22ba831ae4d9f8274de45
Nathan Bodin <bodin.coline@gmail.com>
2021-02-24 21:55:36 +0100

    ref: switch multiple variable to array

--- app/src/main/java/com/example/drinkcounter/MainActivity.java
+++ app/src/main/java/com/example/drinkcounter/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private ButtonMath counting = new ButtonMath();
     private DateSetup ds = new DateSetup();
-    private Integer count1 = 0, count2 = 0, count3 = 0, count4 = 0;
-    private TextView viewCount1, viewCount2, viewCount3, viewCount4;
-    private Boolean showCount1 = false, showCount2 = false, showCount3 = false, showCount4 = false;
+    private Integer[] count = {0, 0, 0, 0};
+    private TextView[] viewCount = new TextView[4];
+    private Boolean[] showCount = {false, false, false, false};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /* Counters setters */
-        viewCount1 = (TextView) findViewById(R.id.count1);
-        viewCount2 = (TextView) findViewById(R.id.count2);
-        viewCount3 = (TextView) findViewById(R.id.count3);
-        viewCount4 = (TextView) findViewById(R.id.count4);
+        viewCount[0] = (TextView) findViewById(R.id.count1);
+        viewCount[1] = (TextView) findViewById(R.id.count2);
+        viewCount[2] = (TextView) findViewById(R.id.count3);
+        viewCount[3] = (TextView) findViewById(R.id.count4);
 
         /* Others to deal with */
         TextView viewDate = (TextView) findViewById(R.id.dateStamp);
@@ -37,64 +37,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-
         switch (view.getId()) {
             case R.id.buttonPlus1:
-                count1 = counting.increment(viewCount1, count1);
-                showCount1 = true;
+                count[0] = counting.increment(viewCount[0], count[0]);
+                showCount[0] = true;
                 break;
 
             case R.id.buttonPlus2:
-                count2 = counting.increment(viewCount2, count2);
-                showCount2 = true;
+                count[1] = counting.increment(viewCount[1], count[1]);
+                showCount[1] = true;
                 break;
 
             case R.id.buttonPlus3:
-                count3 = counting.increment(viewCount3, count3);
-                showCount3 = true;
+                count[2] = counting.increment(viewCount[2], count[2]);
+                showCount[2] = true;
                 break;
 
             case R.id.buttonPlus4:
-                count4 = counting.increment(viewCount4, count4);
-                showCount4 = true;
+                count[3] = counting.increment(viewCount[3], count[3]);
+                showCount[3] = true;
                 break;
 
             case R.id.buttonMinus1:
-                count1 = counting.decrement(viewCount1, count1);
-                showCount1 = true;
+                count[0] = counting.decrement(viewCount[0], count[0]);
+                showCount[0] = true;
                 break;
 
             case R.id.buttonMinus2:
-                count2 = counting.decrement(viewCount2, count2);
-                showCount2 = true;
+                count[1] = counting.decrement(viewCount[1], count[1]);
+                showCount[1] = true;
                 break;
 
             case R.id.buttonMinus3:
-                count3 = counting.decrement(viewCount3, count3);
-                showCount3 = true;
+                count[2] = counting.decrement(viewCount[2], count[2]);
+                showCount[2] = true;
                 break;
 
             case R.id.buttonMinus4:
-                count4 = counting.decrement(viewCount4, count4);
-                showCount4 = true;
+                count[3] = counting.decrement(viewCount[3], count[3]);
+                showCount[3] = true;
                 break;
 
             case R.id.buttonReset:
-                if (showCount1) {
-                    count1 = counting.resetCount(viewCount1, count1);
-                    showCount1 = false;
+                if (showCount[0]) {
+                    count[0] = counting.resetCount(viewCount[0], count[0]);
+                    showCount[0] = false;
                 }
-                if (showCount2) {
-                    count2 = counting.resetCount(viewCount2, count2);
-                    showCount2 = false;
+                if (showCount[1]) {
+                    count[1] = counting.resetCount(viewCount[1], count[1]);
+                    showCount[1] = false;
                 }
-                if (showCount3) {
-                    count3 = counting.resetCount(viewCount3, count3);
-                    showCount3 = false;
+                if (showCount[2]) {
+                    count[2] = counting.resetCount(viewCount[2], count[2]);
+                    showCount[2] = false;
                 }
-                if (showCount4) {
-                    count4 = counting.resetCount(viewCount4, count4);
-                    showCount4 = false;
+                if (showCount[3]) {
+                    count[3] = counting.resetCount(viewCount[3], count[3]);
+                    showCount[4] = false;
                 }
                 break;
 
